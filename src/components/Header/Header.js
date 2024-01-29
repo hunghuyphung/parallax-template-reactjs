67aafc2fb2cabba52a7f9991537e3f66cc75bea0
hungph3 <hungph3@vnpay.vn>
2024-01-29 10:09:39 +0700

    create-header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,37 @@
+import './Header.css';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faBars, faCheckDouble, faEnvelope, faUser} from "@fortawesome/free-solid-svg-icons";
 
 function Header() {
 
+    return (
+        <div id="header">
+            <div className="display-flex flex-space-between">
+                <div>
+                    <a href="#" className="h-button color-dark-gray hover-gray">HOME</a>
+                    <a href="#" className="h-button color-dark-gray hover-gray hide-small">
+                        <FontAwesomeIcon icon={faUser}
+                                         style={{fontSize: '13px', marginBottom: '1px', marginRight: '2px'}}/>
+                        ABOUT
+                    </a>
+                    <a href="#" className="h-button color-dark-gray hover-gray hide-small">
+                        <FontAwesomeIcon icon={faCheckDouble}
+                                         style={{fontSize: '12px', marginBottom: '1px', marginRight: '2px'}}/>
+                        PORTFOLIO
+                    </a>
+                    <a href="#" className="h-button color-dark-gray hover-gray hide-small">
+                        <FontAwesomeIcon icon={faEnvelope} style={{fontSize: '16px', marginRight: '2px'}}/>
+                        CONTACT
+                    </a>
+                </div>
+                <div>
+                    <a href="#" className="h-button color-dark-gray hover-black hide-large show-small">
+                        <FontAwesomeIcon icon={faBars}/>
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Header;
