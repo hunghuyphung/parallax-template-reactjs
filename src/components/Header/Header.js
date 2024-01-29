33e9637834e2c7e466c2c1da2c56a7851a8d524c
hungph3 <hungph3@vnpay.vn>
2024-01-29 10:54:35 +0700

    scroll-nav

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,31 +1,53 @@
 import './Header.css';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faBars, faCheckDouble, faEnvelope, faUser} from "@fortawesome/free-solid-svg-icons";
+import {useEffect, useState} from "react";
 
 function Header() {
+    const [navClasses, setNavClasses] = useState('display-flex flex-space-between');
+    const [color, setColor] = useState({color: '#777'});
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+                setNavClasses('display-flex flex-space-between h-card bc-white animate-top');
+                setColor({color: '#000'});
+            } else {
+                setNavClasses('display-flex flex-space-between');
+                setColor({color: '#777'});
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+
+    }, []);
 
     return (
         <div id="header">
-            <div className="display-flex flex-space-between">
+            <div id="nav-bar-js" className={navClasses} style={{height: '43px'}}>
                 <div>
-                    <a href="#" className="h-button color-dark-gray hover-gray">HOME</a>
-                    <a href="#" className="h-button color-dark-gray hover-gray hide-small">
+                    <a href="#" className="h-button hover-gray" style={color}>HOME</a>
+                    <a href="#" className="h-button hover-gray hide-small" style={color}>
                         <FontAwesomeIcon icon={faUser}
                                          style={{fontSize: '13px', marginBottom: '1px', marginRight: '2px'}}/>
                         ABOUT
                     </a>
-                    <a href="#" className="h-button color-dark-gray hover-gray hide-small">
+                    <a href="#" className="h-button hover-gray hide-small" style={color}>
                         <FontAwesomeIcon icon={faCheckDouble}
                                          style={{fontSize: '12px', marginBottom: '1px', marginRight: '2px'}}/>
                         PORTFOLIO
                     </a>
-                    <a href="#" className="h-button color-dark-gray hover-gray hide-small">
+                    <a href="#" className="h-button hover-gray hide-small" style={color}>
                         <FontAwesomeIcon icon={faEnvelope} style={{fontSize: '16px', marginRight: '2px'}}/>
                         CONTACT
                     </a>
                 </div>
                 <div>
-                    <a href="#" className="h-button color-dark-gray hover-black hide-large show-small">
+                    <a href="#" className="h-button hover-black color-dark-gray hide-large show-small">
                         <FontAwesomeIcon icon={faBars}/>
                     </a>
                 </div>
